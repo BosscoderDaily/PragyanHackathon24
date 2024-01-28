3044bc4d454cfdff74ac5c6ecf7dc9dd5d1ac0a8
Boss_{coder}:"daily <37211017+BosscoderDaily@users.noreply.github.com>
2024-01-28 13:25:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function renderPredictions(predictions, canvas, carCountSpan) {
 
           // Count recognized cars
           carCount++;
+       google.script.run.doPost(carCount);
       }
   });
 
